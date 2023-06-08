5af184914194f277ac26c012274d81004899103c
Pushpendra Sharma <pushpendrasharma400@gmail.com>
2023-06-09 01:05:21 +0530

    fix: update Header as resuable component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import Image from 'next/image';
-import heroImage from '@/images/hero.svg';
 
-export default function Header() {
+import heroImage from '@/images/hero.svg';
+import { HeaderT } from '@/types';
+
+export default function Header({
+  description,
+  heading,
+  image,
+  subHeading,
+}: HeaderT) {
   return (
     <header
       className='flex flex-col justify-evenly items-center gap-2 flex-1 m-0 px-4 py-8 box-border'
       data-testid='header'
     >
-      <Image
-        alt='hero-image'
-        className='max-w-[80%] md:max-w-sm block m-auto'
-        src={heroImage}
-      />
-      <h1 className='text-2xl text-center text-gray-800 mt-4 p-4'>
-        Hi, I am <span className='text-emerald-700'>Pushpendra</span>
+      {image && (
+        <Image
+          alt='hero-image'
+          className='max-w-[80%] md:max-w-sm block m-auto'
+          src={heroImage}
+          data-testid='hero-image'
+        />
+      )}
+      <h1
+        className='text-2xl text-center text-gray-800 mt-4 p-4'
+        data-testid='hero-heading'
+      >
+        {heading}
+        {subHeading && <span className='text-emerald-700'>{subHeading}</span>}
       </h1>
-      <p className='m-0 p-2 text-justify max-w-2xl'>
-        I am a Frontend developer.
+      <p
+        className='m-0 p-2 text-justify max-w-2xl'
+        data-testid='hero-description'
+      >
+        {description}
       </p>
     </header>
   );
--- components/__tests__/Header.test.tsx
+++ components/__tests__/Header.test.tsx
@@ -3,10 +3,23 @@ import '@testing-library/jest-dom';
 import Header from '../Header';
 
 describe('Header', () => {
-  it('renders the header component', () => {
-    render(<Header />);
+  it('renders the header component without image and subheading', () => {
+    render(
+      <Header
+        heading='Technologies'
+        description="I'm familiar with HTML5, CSS3, Git, JavaScript, NodeJS, ReactJS, TailwindCSS, TypeScript and Web Hosting."
+      />
+    );
 
     const headerElement = screen.getByTestId('header');
     expect(headerElement).toBeInTheDocument();
+
+    expect(screen.getByTestId('hero-heading')).toHaveTextContent(
+      'Technologies'
+    );
+
+    expect(screen.getByTestId('hero-description')).toHaveTextContent(
+      "I'm familiar with HTML5, CSS3, Git, JavaScript, NodeJS, ReactJS, TailwindCSS, TypeScript and Web Hosting."
+    );
   });
 });
